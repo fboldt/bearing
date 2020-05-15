8f8439c39fe1fd98af07167494d58ec2d3e9ebb5
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2020-05-14 23:09:25 -0300

    Update cwru.py

--- cwru.py
+++ cwru.py
@@ -1,146 +1,148 @@
-matlab_files_name = {}
-# Normal
-matlab_files_name["Normal_0"] = "97.mat"
-matlab_files_name["Normal_1"] = "98.mat"
-matlab_files_name["Normal_2"] = "99.mat"
-matlab_files_name["Normal_3"] = "100.mat"
-# DE Inner Race 0.007 inches
-matlab_files_name["DEIR.007_0"] = "105.mat"
-matlab_files_name["DEIR.007_1"] = "106.mat"
-matlab_files_name["DEIR.007_2"] = "107.mat"
-matlab_files_name["DEIR.007_3"] = "108.mat"
-# DE Ball 0.007 inches
-matlab_files_name["DEB.007_0"] = "118.mat"
-matlab_files_name["DEB.007_1"] = "119.mat"
-matlab_files_name["DEB.007_2"] = "120.mat"
-matlab_files_name["DEB.007_3"] = "121.mat"
-# DE Outer race 0.007 inches centered @6:00
-matlab_files_name["DEOR.007@6_0"] = "130.mat"
-matlab_files_name["DEOR.007@6_1"] = "131.mat"
-matlab_files_name["DEOR.007@6_2"] = "132.mat"
-matlab_files_name["DEOR.007@6_3"] = "133.mat"
-# DE Outer race 0.007 inches centered @3:00
-matlab_files_name["DEOR.007@3_0"] = "144.mat"
-matlab_files_name["DEOR.007@3_1"] = "145.mat"
-matlab_files_name["DEOR.007@3_2"] = "146.mat"
-matlab_files_name["DEOR.007@3_3"] = "147.mat"
-# DE Outer race 0.007 inches centered @12:00
-matlab_files_name["DEOR.007@12_0"] = "156.mat"
-matlab_files_name["DEOR.007@12_1"] = "158.mat"
-matlab_files_name["DEOR.007@12_2"] = "159.mat"
-matlab_files_name["DEOR.007@12_3"] = "160.mat"
-# DE Inner Race 0.014 inches
-matlab_files_name["DEIR.014_0"] = "169.mat"
-matlab_files_name["DEIR.014_1"] = "170.mat"
-matlab_files_name["DEIR.014_2"] = "171.mat"
-matlab_files_name["DEIR.014_3"] = "172.mat"
-# DE Ball 0.014 inches
-matlab_files_name["DEB.014_0"] = "185.mat"
-matlab_files_name["DEB.014_1"] = "186.mat"
-matlab_files_name["DEB.014_2"] = "187.mat"
-matlab_files_name["DEB.014_3"] = "188.mat"
-# DE Outer race 0.014 inches centered @6:00
-matlab_files_name["DEOR.014@6_0"] = "197.mat"
-matlab_files_name["DEOR.014@6_1"] = "198.mat"
-matlab_files_name["DEOR.014@6_2"] = "199.mat"
-matlab_files_name["DEOR.014@6_3"] = "200.mat"
-# DE Ball 0.021 inches
-matlab_files_name["DEB.021_0"] = "222.mat"
-matlab_files_name["DEB.021_1"] = "223.mat"
-matlab_files_name["DEB.021_2"] = "224.mat"
-matlab_files_name["DEB.021_3"] = "225.mat"
-# FE Inner Race 0.021 inches
-matlab_files_name["FEIR.021_0"] = "270.mat"
-matlab_files_name["FEIR.021_1"] = "271.mat"
-matlab_files_name["FEIR.021_2"] = "272.mat"
-matlab_files_name["FEIR.021_3"] = "273.mat"
-# FE Inner Race 0.014 inches
-matlab_files_name["FEIR.014_0"] = "274.mat"
-matlab_files_name["FEIR.014_1"] = "275.mat"
-matlab_files_name["FEIR.014_2"] = "276.mat"
-matlab_files_name["FEIR.014_3"] = "277.mat"
-# FE Ball 0.007 inches
-matlab_files_name["FEB.007_0"] = "282.mat"
-matlab_files_name["FEB.007_1"] = "283.mat"
-matlab_files_name["FEB.007_2"] = "284.mat"
-matlab_files_name["FEB.007_3"] = "285.mat"
-# DE Inner Race 0.021 inches
-matlab_files_name["DEIR.021_0"] = "209.mat"
-matlab_files_name["DEIR.021_1"] = "210.mat"
-matlab_files_name["DEIR.021_2"] = "211.mat"
-matlab_files_name["DEIR.021_3"] = "212.mat"
-# DE Outer race 0.021 inches centered @6:00
-matlab_files_name["DEOR.021@6_0"] = "234.mat"
-matlab_files_name["DEOR.021@6_1"] = "235.mat"
-matlab_files_name["DEOR.021@6_2"] = "236.mat"
-matlab_files_name["DEOR.021@6_3"] = "237.mat"
-# DE Outer race 0.021 inches centered @3:00
-matlab_files_name["DEOR.021@3_0"] = "246.mat"
-matlab_files_name["DEOR.021@3_1"] = "247.mat"
-matlab_files_name["DEOR.021@3_2"] = "248.mat"
-matlab_files_name["DEOR.021@3_3"] = "249.mat"
-# DE Outer race 0.021 inches centered @12:00
-matlab_files_name["DEOR.021@12_0"] = "258.mat"
-matlab_files_name["DEOR.021@12_1"] = "259.mat"
-matlab_files_name["DEOR.021@12_2"] = "260.mat"
-matlab_files_name["DEOR.021@12_3"] = "261.mat"
-# FE Inner Race 0.007 inches
-matlab_files_name["FEIR.007_0"] = "278.mat"
-matlab_files_name["FEIR.007_1"] = "279.mat"
-matlab_files_name["FEIR.007_2"] = "280.mat"
-matlab_files_name["FEIR.007_3"] = "281.mat"
-# FE Ball 0.014 inches
-matlab_files_name["FEB.014_0"] = "286.mat"
-matlab_files_name["FEB.014_1"] = "287.mat"
-matlab_files_name["FEB.014_2"] = "288.mat"
-matlab_files_name["FEB.014_3"] = "289.mat"
-# FE Ball 0.021 inches
-matlab_files_name["FEB.021_0"] = "290.mat"
-matlab_files_name["FEB.021_1"] = "291.mat"
-matlab_files_name["FEB.021_2"] = "292.mat"
-matlab_files_name["FEB.021_3"] = "293.mat"
-# FE Outer race 0.007 inches centered @6:00
-matlab_files_name["FEOR.007@6_0"] = "294.mat"
-matlab_files_name["FEOR.007@6_1"] = "295.mat"
-matlab_files_name["FEOR.007@6_2"] = "296.mat"
-matlab_files_name["FEOR.007@6_3"] = "297.mat"
-# FE Outer race 0.007 inches centered @3:00
-matlab_files_name["FEOR.007@3_0"] = "298.mat"
-matlab_files_name["FEOR.007@3_1"] = "299.mat"
-matlab_files_name["FEOR.007@3_2"] = "300.mat"
-matlab_files_name["FEOR.007@3_3"] = "301.mat"
-# FE Outer race 0.007 inches centered @12:00
-matlab_files_name["FEOR.007@12_0"] = "302.mat"
-matlab_files_name["FEOR.007@12_1"] = "305.mat"
-matlab_files_name["FEOR.007@12_2"] = "306.mat"
-matlab_files_name["FEOR.007@12_3"] = "307.mat"
-# FE Outer race 0.014 inches centered @3:00
-matlab_files_name["FEOR.014@3_0"] = "310.mat"
-matlab_files_name["FEOR.014@3_1"] = "309.mat"
-matlab_files_name["FEOR.014@3_2"] = "311.mat"
-matlab_files_name["FEOR.014@3_3"] = "312.mat"
-# FE Outer race 0.014 inches centered @6:00
-matlab_files_name["FEOR.014@6_0"] = "313.mat"
-# FE Outer race 0.021 inches centered @6:00
-matlab_files_name["FEOR.021@6_0"] = "315.mat"
-# FE Outer race 0.021 inches centered @3:00
-matlab_files_name["FEOR.021@3_1"] = "316.mat"
-matlab_files_name["FEOR.021@3_2"] = "317.mat"
-matlab_files_name["FEOR.021@3_3"] = "318.mat"
-# DE Inner Race 0.028 inches
-matlab_files_name["DEIR.028_0"] = "3001.mat"
-matlab_files_name["DEIR.028_1"] = "3002.mat"
-matlab_files_name["DEIR.028_2"] = "3003.mat"
-matlab_files_name["DEIR.028_3"] = "3004.mat"
-# DE Ball 0.028 inches
-matlab_files_name["DEB.028_0"] = "3005.mat"
-matlab_files_name["DEB.028_1"] = "3006.mat"
-matlab_files_name["DEB.028_2"] = "3007.mat"
-matlab_files_name["DEB.028_3"] = "3008.mat"
-
 import urllib.request
 import os.path
-def download_cwrufiles(matlab_files_name):
+
+def download_database():
+  
+  matlab_files_name = {}
+  # Normal
+  matlab_files_name["Normal_0"] = "97.mat"
+  matlab_files_name["Normal_1"] = "98.mat"
+  matlab_files_name["Normal_2"] = "99.mat"
+  matlab_files_name["Normal_3"] = "100.mat"
+  # DE Inner Race 0.007 inches
+  matlab_files_name["DEIR.007_0"] = "105.mat"
+  matlab_files_name["DEIR.007_1"] = "106.mat"
+  matlab_files_name["DEIR.007_2"] = "107.mat"
+  matlab_files_name["DEIR.007_3"] = "108.mat"
+  # DE Ball 0.007 inches
+  matlab_files_name["DEB.007_0"] = "118.mat"
+  matlab_files_name["DEB.007_1"] = "119.mat"
+  matlab_files_name["DEB.007_2"] = "120.mat"
+  matlab_files_name["DEB.007_3"] = "121.mat"
+  # DE Outer race 0.007 inches centered @6:00
+  matlab_files_name["DEOR.007@6_0"] = "130.mat"
+  matlab_files_name["DEOR.007@6_1"] = "131.mat"
+  matlab_files_name["DEOR.007@6_2"] = "132.mat"
+  matlab_files_name["DEOR.007@6_3"] = "133.mat"
+  # DE Outer race 0.007 inches centered @3:00
+  matlab_files_name["DEOR.007@3_0"] = "144.mat"
+  matlab_files_name["DEOR.007@3_1"] = "145.mat"
+  matlab_files_name["DEOR.007@3_2"] = "146.mat"
+  matlab_files_name["DEOR.007@3_3"] = "147.mat"
+  # DE Outer race 0.007 inches centered @12:00
+  matlab_files_name["DEOR.007@12_0"] = "156.mat"
+  matlab_files_name["DEOR.007@12_1"] = "158.mat"
+  matlab_files_name["DEOR.007@12_2"] = "159.mat"
+  matlab_files_name["DEOR.007@12_3"] = "160.mat"
+  # DE Inner Race 0.014 inches
+  matlab_files_name["DEIR.014_0"] = "169.mat"
+  matlab_files_name["DEIR.014_1"] = "170.mat"
+  matlab_files_name["DEIR.014_2"] = "171.mat"
+  matlab_files_name["DEIR.014_3"] = "172.mat"
+  # DE Ball 0.014 inches
+  matlab_files_name["DEB.014_0"] = "185.mat"
+  matlab_files_name["DEB.014_1"] = "186.mat"
+  matlab_files_name["DEB.014_2"] = "187.mat"
+  matlab_files_name["DEB.014_3"] = "188.mat"
+  # DE Outer race 0.014 inches centered @6:00
+  matlab_files_name["DEOR.014@6_0"] = "197.mat"
+  matlab_files_name["DEOR.014@6_1"] = "198.mat"
+  matlab_files_name["DEOR.014@6_2"] = "199.mat"
+  matlab_files_name["DEOR.014@6_3"] = "200.mat"
+  # DE Ball 0.021 inches
+  matlab_files_name["DEB.021_0"] = "222.mat"
+  matlab_files_name["DEB.021_1"] = "223.mat"
+  matlab_files_name["DEB.021_2"] = "224.mat"
+  matlab_files_name["DEB.021_3"] = "225.mat"
+  # FE Inner Race 0.021 inches
+  matlab_files_name["FEIR.021_0"] = "270.mat"
+  matlab_files_name["FEIR.021_1"] = "271.mat"
+  matlab_files_name["FEIR.021_2"] = "272.mat"
+  matlab_files_name["FEIR.021_3"] = "273.mat"
+  # FE Inner Race 0.014 inches
+  matlab_files_name["FEIR.014_0"] = "274.mat"
+  matlab_files_name["FEIR.014_1"] = "275.mat"
+  matlab_files_name["FEIR.014_2"] = "276.mat"
+  matlab_files_name["FEIR.014_3"] = "277.mat"
+  # FE Ball 0.007 inches
+  matlab_files_name["FEB.007_0"] = "282.mat"
+  matlab_files_name["FEB.007_1"] = "283.mat"
+  matlab_files_name["FEB.007_2"] = "284.mat"
+  matlab_files_name["FEB.007_3"] = "285.mat"
+  # DE Inner Race 0.021 inches
+  matlab_files_name["DEIR.021_0"] = "209.mat"
+  matlab_files_name["DEIR.021_1"] = "210.mat"
+  matlab_files_name["DEIR.021_2"] = "211.mat"
+  matlab_files_name["DEIR.021_3"] = "212.mat"
+  # DE Outer race 0.021 inches centered @6:00
+  matlab_files_name["DEOR.021@6_0"] = "234.mat"
+  matlab_files_name["DEOR.021@6_1"] = "235.mat"
+  matlab_files_name["DEOR.021@6_2"] = "236.mat"
+  matlab_files_name["DEOR.021@6_3"] = "237.mat"
+  # DE Outer race 0.021 inches centered @3:00
+  matlab_files_name["DEOR.021@3_0"] = "246.mat"
+  matlab_files_name["DEOR.021@3_1"] = "247.mat"
+  matlab_files_name["DEOR.021@3_2"] = "248.mat"
+  matlab_files_name["DEOR.021@3_3"] = "249.mat"
+  # DE Outer race 0.021 inches centered @12:00
+  matlab_files_name["DEOR.021@12_0"] = "258.mat"
+  matlab_files_name["DEOR.021@12_1"] = "259.mat"
+  matlab_files_name["DEOR.021@12_2"] = "260.mat"
+  matlab_files_name["DEOR.021@12_3"] = "261.mat"
+  # FE Inner Race 0.007 inches
+  matlab_files_name["FEIR.007_0"] = "278.mat"
+  matlab_files_name["FEIR.007_1"] = "279.mat"
+  matlab_files_name["FEIR.007_2"] = "280.mat"
+  matlab_files_name["FEIR.007_3"] = "281.mat"
+  # FE Ball 0.014 inches
+  matlab_files_name["FEB.014_0"] = "286.mat"
+  matlab_files_name["FEB.014_1"] = "287.mat"
+  matlab_files_name["FEB.014_2"] = "288.mat"
+  matlab_files_name["FEB.014_3"] = "289.mat"
+  # FE Ball 0.021 inches
+  matlab_files_name["FEB.021_0"] = "290.mat"
+  matlab_files_name["FEB.021_1"] = "291.mat"
+  matlab_files_name["FEB.021_2"] = "292.mat"
+  matlab_files_name["FEB.021_3"] = "293.mat"
+  # FE Outer race 0.007 inches centered @6:00
+  matlab_files_name["FEOR.007@6_0"] = "294.mat"
+  matlab_files_name["FEOR.007@6_1"] = "295.mat"
+  matlab_files_name["FEOR.007@6_2"] = "296.mat"
+  matlab_files_name["FEOR.007@6_3"] = "297.mat"
+  # FE Outer race 0.007 inches centered @3:00
+  matlab_files_name["FEOR.007@3_0"] = "298.mat"
+  matlab_files_name["FEOR.007@3_1"] = "299.mat"
+  matlab_files_name["FEOR.007@3_2"] = "300.mat"
+  matlab_files_name["FEOR.007@3_3"] = "301.mat"
+  # FE Outer race 0.007 inches centered @12:00
+  matlab_files_name["FEOR.007@12_0"] = "302.mat"
+  matlab_files_name["FEOR.007@12_1"] = "305.mat"
+  matlab_files_name["FEOR.007@12_2"] = "306.mat"
+  matlab_files_name["FEOR.007@12_3"] = "307.mat"
+  # FE Outer race 0.014 inches centered @3:00
+  matlab_files_name["FEOR.014@3_0"] = "310.mat"
+  matlab_files_name["FEOR.014@3_1"] = "309.mat"
+  matlab_files_name["FEOR.014@3_2"] = "311.mat"
+  matlab_files_name["FEOR.014@3_3"] = "312.mat"
+  # FE Outer race 0.014 inches centered @6:00
+  matlab_files_name["FEOR.014@6_0"] = "313.mat"
+  # FE Outer race 0.021 inches centered @6:00
+  matlab_files_name["FEOR.021@6_0"] = "315.mat"
+  # FE Outer race 0.021 inches centered @3:00
+  matlab_files_name["FEOR.021@3_1"] = "316.mat"
+  matlab_files_name["FEOR.021@3_2"] = "317.mat"
+  matlab_files_name["FEOR.021@3_3"] = "318.mat"
+  # DE Inner Race 0.028 inches
+  matlab_files_name["DEIR.028_0"] = "3001.mat"
+  matlab_files_name["DEIR.028_1"] = "3002.mat"
+  matlab_files_name["DEIR.028_2"] = "3003.mat"
+  matlab_files_name["DEIR.028_3"] = "3004.mat"
+  # DE Ball 0.028 inches
+  matlab_files_name["DEB.028_0"] = "3005.mat"
+  matlab_files_name["DEB.028_1"] = "3006.mat"
+  matlab_files_name["DEB.028_2"] = "3007.mat"
+  matlab_files_name["DEB.028_3"] = "3008.mat"
+  
   url="http://csegroups.case.edu/sites/default/files/bearingdatacenter/files/Datafiles/"
   n = len(matlab_files_name)
   for i,key in enumerate(matlab_files_name):
@@ -149,4 +151,3 @@ def download_cwrufiles(matlab_files_name):
       urllib.request.urlretrieve(url+file_name, file_name)
     print("{}/{}\t{}\t{}".format(i+1, n, key, file_name))
 
-download_cwrufiles(matlab_files_name)
