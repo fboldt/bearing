87bd0b6afed872bcdd634a5afc29c46c4378196c
lucioventurim <34462880+lucioventurim@users.noreply.github.com>
2020-06-03 23:25:09 -0300

    Update ims.py
    Changed dir names and included debug mode.

--- ims.py
+++ ims.py
@@ -25,6 +25,49 @@ from pyunpack import Archive
 #!pip install patool
 #!from pyunpack import Archive
 
+def files_debug(dirfiles):
+  """
+  Associate each Matlab file name to a bearing condition in a Python dictionary. 
+  The dictionary keys identify the conditions.
+  
+  NOTE: Used only for debug. Use "debug=1" on intialization.
+  """
+  files_names = {}
+  
+  first_test_path = os.path.join(dirfiles,"1st_test")
+  fourth_test_path = os.path.join(dirfiles,"4th_test","txt")
+
+  first_test_files = [f for f in listdir(first_test_path) if isfile(join(first_test_path, f))]
+  first_test_files.sort()
+  print(first_test_files)
+  
+  fourth_test_files = [f for f in listdir(fourth_test_path) if isfile(join(fourth_test_path, f))]
+  fourth_test_files.sort()
+  print(fourth_test_files)
+  
+  n = 100 # Fewer files, compared to the original 750
+
+  # Normal
+  for i in range(1, n+1):
+    key = "Normal_" + str(i).zfill(3)
+    files_names[key] = first_test_files[i-1]
+
+  # Inner
+  for i in range(1370, n+1370):
+    key = "IR_" + str(i).zfill(3)
+    files_names[key] = first_test_files[i-1]
+
+  # Ball
+  for i in range(1370, n+1370):
+    key = "BA_" + str(i).zfill(3)
+    files_names[key] = first_test_files[i-1]
+
+  # Outer
+  for i in range(5575, n+5575):
+    key = "OR_" + str(i).zfill(3)
+    files_names[key] = fourth_test_files[i-1]
+
+  return files_names
 
 class IMS(database.Database):
   """
@@ -49,10 +92,10 @@ class IMS(database.Database):
   segmentate()
     Semgmentate the raw files in various .csv files
   """
-  def __init__(self):
-    #self.files = files_IMS()
-    self.rawfilesdir = "database_raw"
-    self.dirdest = "database"
+  def __init__(self, debug=0):
+    self.debug=debug
+    self.rawfilesdir = "ims_raw"
+    self.dirdest = "ims_seg"
     self.url="https://ti.arc.nasa.gov/c/3/"
 
   def download(self):
@@ -72,8 +115,12 @@ class IMS(database.Database):
     Archive('IMS.7z').extractall(dirname)
     Archive(os.path.join(dirname, '1st_test.rar')).extractall(dirname)
     Archive(os.path.join(dirname, '3rd_test.rar')).extractall(dirname)
-    files_names = files_IMS(dirname)
 
+    if self.debug==0:
+      files_names = files_IMS(dirname)
+    else:
+      files_names = files_debug(dirname)
+    
     print(files_names)
     self.files = files_names
     
